ab4309ec7b678c6f4de976347b2e3992d1184315
mikkokallio <mikko.m.kallio@gmail.com>
2018-06-27 16:04:01 +0300

    text output is prettier still

--- src/fi/academy/Controller.java
+++ src/fi/academy/Controller.java
@@ -30,15 +30,27 @@ public class Controller {
     @FXML
     protected void parseCommand(ActionEvent event) {
         output.getChildren().clear();
+
+        // Get command from text box, parse it
         String input = commandInput.getText();
         commandParser(input);
         output.getChildren().addAll((new Text("> "+input+"\n\n")));
+
+        // If G.R.U.E. has been awakened, decrease the timer
+        if (flaglist.contains("shitHitTheFan")) {
+            System.out.println("Shit has hit the fan!");
+            // decrease timer THEN if 0, monster appears!
+        }
+
+        // If previous command triggered an item event, activate the event
         if (!lastItemUsed.equals(" ")) {
-            Text t = new Text(Game.encounterHappens(lastItemUsed));
+            Text t = new Text(Game.encounterHappens(lastItemUsed)+"\n\n");
             t.setStyle("-fx-font-style: italic");
             output.getChildren().addAll(t);
             lastItemUsed = " ";
         }
+
+        // If current area has encounters and their conditions are true, activate the encounters
         Area thisArea = areaList.get(currentArea);
         if(!thisArea.getEncountersinRoom().contains(".")) {
             for (int i = 0; i < (thisArea.getEncountersinRoom().size()); i++) {
@@ -47,21 +59,24 @@ public class Controller {
                     text5.setStyle("-fx-font-style: italic");
                     output.getChildren().addAll(text5, new Text ("\n\n"));
                 }
-                ;
             }
         }
+
         String borderAreas = "";
         String allItems = "";
         for (int i = 0; i < thisArea.getBorderingAreas().size(); i++) {
-            borderAreas += "  " + fetchAreaName(thisArea.getBorderingAreas().get(i));
+            borderAreas += fetchAreaName(thisArea.getBorderingAreas().get(i));
+            if (i < thisArea.getBorderingAreas().size()-1) borderAreas += ", ";
         }
 
         for (int i = 0; i < thisArea.getItemList().size(); i++) {
-            allItems += "  " + thisArea.getItemList().get(i);
+            allItems += thisArea.getItemList().get(i);
+            if (i < thisArea.getItemList().size()-1) allItems += ", ";
         }
 
-        Text text1 = new Text("You are in "+thisArea.getAreaName()+"\n");
-        Text text2 = new Text(thisArea.getDescription()+"\n");
+        Text text1 = new Text("You are in "+thisArea.getAreaName()+".\n\n");
+        text1.setStyle("-fx-font-weight: bold");
+        Text text2 = new Text(thisArea.getDescription()+"\n\n");
         Text text3 = new Text("Adjacent areas: "+borderAreas+"\n");
         Text text4 = new Text("Items in room: "+allItems+"\n");
         output.getChildren().addAll(text1, text2, text3, text4, new Text("\n\n"));
@@ -78,7 +93,7 @@ public class Controller {
         }
         // This is ONLY for debugging purposes! Not intended for players to see!
         for (String flag : Game.flaglist) {
-            inventory.getChildren().addAll(new Text(flag+"\n"));
+            inventory.getChildren().addAll(new Text("Flag: "+flag+"\n"));
         }
     }
 }
