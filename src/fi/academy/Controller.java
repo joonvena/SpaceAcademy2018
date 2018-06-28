16be1ff0fa0154f97e334da46b3e449255937d90
joonvena <joonvena@gmail.com>
2018-06-28 12:36:02 +0300

    Modifications to the UI and map

--- src/fi/academy/Controller.java
+++ src/fi/academy/Controller.java
@@ -10,13 +10,20 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
-
+import javafx.scene.image.Image;
+import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static fi.academy.Game.*;
 
 public class Controller {
+	
+	// Custom colors for elements
+    Color locationColor = Color.web("#81CFE0");
+    Color itemColor = Color.web("#59ABE3");
+    Color encounterColor = Color.web("#3498DB");
+	
     @FXML
     private TextField commandInput;
     @FXML
@@ -27,15 +34,21 @@ public class Controller {
     private Canvas canvas;
     @FXML
     private TextFlow inventory;
-
     @FXML
     private ScrollPane Scroll;
+	@FXML
+    private ScrollPane inventoryScroll;
+    @FXML
+    private javafx.scene.image.ImageView ImageResolve;
+    @FXML
+    private javafx.scene.image.ImageView eventPicture;
 
     @FXML
     protected void parseCommand(ActionEvent event) {
         output.heightProperty().addListener(observable -> Scroll.setVvalue(1D));
         Scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-
+		inventoryScroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+		
         // Get command from text box, parse it
         String input = commandInput.getText();
         commandParser(input);
@@ -124,21 +137,69 @@ public class Controller {
         }
 
         Text text1 = new Text("\nYou are in: ");
-        Text text5 = new Text(thisArea.getAreaName()+"\n");
-        Text text2 = new Text(thisArea.getDescription()+"\n");
-        Text text3 = new Text("Adjacent areas: "+borderAreas+"\n");
-        Text text4 = new Text("Items in room: "+allItems+"\n");
+        Text text5 = new Text(thisArea.getAreaName() + "\n\n");
+        Text text2 = new Text(thisArea.getDescription() + "\n\n");
+        Text text3 = new Text("Adjacent areas: ");
+        Text areaStyling = new Text(borderAreas + "\n\n");
+        Text text4 = new Text("Items in room: ");
+        Text itemsStyling = new Text(allItems + "\n");
+
+
+        // Updating the location on map
+        if (thisArea.getAreaName().equals("Cryogenics")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_startingpoint.jpg"));
+        } else if (thisArea.getAreaName().equals("Hydroponics")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_hydro.jpg"));
+        } else if (thisArea.getAreaName().equals("Bio Lab")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_biolab.jpg"));
+        } else if (thisArea.getAreaName().equals("Med Bay")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_medical.jpg"));
+        } else if (thisArea.getAreaName().equals("Access Hub")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_access.jpg"));
+        } else if (thisArea.getAreaName().equals("Rec Room")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_rec.jpg"));
+        } else if (thisArea.getAreaName().equals("Restrooms")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_restroom.jpg"));
+        } else if (thisArea.getAreaName().equals("Gym")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_gym.jpg"));
+        } else if (thisArea.getAreaName().equals("Crew Quarters")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_crew.jpg"));
+        } else if (thisArea.getAreaName().equals("Robotics")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_robotic.jpg"));
+        } else if (thisArea.getAreaName().equals("FIXServer Room")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_server.jpg"));
+        } else if (thisArea.getAreaName().equals("FIXReactor")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_reactor.jpg"));
+        } else if (thisArea.getAreaName().equals("FIXEngine Room")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_engine.jpg"));
+        } else if (thisArea.getAreaName().equals("FIXComms")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_comms.jpg"));
+        } else if (thisArea.getAreaName().equals("FIXAstrogation")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_astro.jpg"));
+        } else if (thisArea.getAreaName().equals("Storage")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_storage.jpg"));
+        } else if (thisArea.getAreaName().equals("Airlock")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_airlock.jpg"));
+        } else if (thisArea.getAreaName().equals("Escape Pod")) {
+            ImageResolve.setImage(new Image("/fi/academy/mapimages/map_pod.jpg"));
+        }
+
+
         text1.setFill(Color.WHITE);
         text1.setStyle("-fx-font-weight: bold; -fx-font-style: italic;");
-        text5.setFill(Color.RED);
+        text5.setFill(locationColor);
         text5.setStyle("-fx-font-weight: bold;");
         text2.setFill(Color.WHITE);
+        text2.setStyle("-fx-font-style: italic; -fx-font-size: 16px;");
         text3.setFill(Color.WHITE);
+        text3.setStyle("-fx-font-weight: bold;");
+        areaStyling.setFill(Color.WHITE);
         text4.setFill(Color.WHITE);
         text4.setStyle("-fx-font-weight: bold");
+        itemsStyling.setFill(itemColor);
 
         //output.getChildren().addAll(text1, text2, text3, text4, new Text("\n\n"));
-        output.getChildren().addAll(text1, text5, text2, text3, text4, new Text("\n\n"));
+        output.getChildren().addAll(text1, text5, text2, text3, areaStyling, text4, itemsStyling, new Text("\n\n"));
     }
 
     public void updateGUI() {
@@ -146,6 +207,7 @@ public class Controller {
         inventory.getChildren().clear();
         System.out.println("This is test");
         System.out.println(Game.inventory);
+		inventory.heightProperty().addListener(observable -> inventoryScroll.setVvalue(1D));
         for (String item : Game.inventory) {
             inventory.getChildren().addAll(new Text(item+"\n"));
         }
